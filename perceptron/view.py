a56608dfac4148150648a0e6c6cc3885bbbbdc0c
Serhiy Latyuk <serhiy_latyuk@inbox.ru>
2018-12-12 14:38:05 +0200

    add eigenvalues checking for ellipsoid classification

--- perceptron/view.py
+++ perceptron/view.py
@@ -3,12 +3,12 @@ from utils import DatasetGenerator
 from perceptron import PerceptronClassifier
 import numpy as np
 
-discr_func = 'ellipse'
+discr_func = 'ellipse'          # choose one of ['linear', 'ellipse', 'quadratic']
 data_generator = DatasetGenerator()
-X, y = data_generator.generate_separable_data(num_points=2000, discr_func=discr_func)
+X, y = data_generator.generate_separable_data(num_points=57, discr_func=discr_func)
 
-# clf = PerceptronClassifier(discr_func=discr_func)
-# clf.fit(X, y)
+clf = PerceptronClassifier(discr_func=discr_func)
+clf.fit(X, y)
 
 plt.xlim(-14, 14)
 plt.ylim(-14, 14)
@@ -21,6 +21,6 @@ xlist = np.linspace(xmin, xmax, 100)
 ylist = np.linspace(ymin, ymax, 100)
 XX, YY = np.meshgrid(xlist, ylist)
 xy = np.vstack([XX.ravel(), YY.ravel()]).T
-# Z = clf.score(xy).reshape(XX.shape)
-# plt.contour(XX, YY, Z, levels=[0], colors=['r'])
+Z = clf.score(xy).reshape(XX.shape)
+plt.contour(XX, YY, Z, levels=[0], colors=['r'])
 plt.show()
