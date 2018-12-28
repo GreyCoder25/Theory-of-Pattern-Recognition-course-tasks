e452d00ff0e7ea92751aa2ed052e97ca58753a4d
Serhiy Latyuk <serhiy_latyuk@inbox.ru>
2018-12-28 15:40:41 +0200

    small bugfix

--- EM/view.py
+++ EM/view.py
@@ -5,17 +5,17 @@ from data_generators import generate_data
 from scipy.stats import norm
 from gaussian_mixture import GaussianMixture
 
-k = 2
-mu = [-2.5, 1]
-sigma = [1, 1]
-priors = [.5, .5]
+k = 3
+mu = [-3, 0, 3]
+sigma = [1, 1, 1]
+priors = [.33, .33, .34]
 num_points = 100
 components = [norm(mu[idx], sigma[idx]) for idx in range(k)]
-data, pdfs = generate_data(num_points, 'gaussian', num_states=2, mu=mu, sigma=sigma, priors=priors)
+data, pdfs = generate_data(num_points, 'gaussian', num_states=k, mu=mu, sigma=sigma, priors=priors)
 
-initial_mu = [-1, 1]
-initial_sigma = [1, 1]
-initial_priors = [.5, .5]
+initial_mu = [-3, 0, 3]
+initial_sigma = [1, 1, 1]
+initial_priors = [.33, .33, .34]
 em = GaussianMixture(k, mu=initial_mu, sigma=initial_sigma, priors=initial_priors)
 em.fit(data)
 pred_priors, (pred_mu, pred_sigma) = em.predict()
