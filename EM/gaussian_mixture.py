5a538051be7d41406e216921367088364327d80c
Serhiy Latyuk <serhiy_latyuk@inbox.ru>
2018-12-28 15:38:42 +0200

    add EM (Gaussian Mixture) implementation

--- EM/gaussian_mixture.py
+++ EM/gaussian_mixture.py
@@ -6,12 +6,62 @@ class GaussianMixture:
 
     def __init__(self, num_states, **param_inits):
         self._k = num_states
-        self._mu, self._sigma, self_priors = param_inits
+        self._mu, self._sigma, self._priors = param_inits.values()
+        self._epsilon = 0.001
+        # casting array-like parameters to ndarrays
+        self._mu = np.array(self._mu)
+        self._sigma = np.array(self._sigma)
+        self._priors = np.array(self._priors)
+        self._gaussians = [norm(mu, sigma) for mu, sigma in zip(self._mu, self._sigma)]
 
     def fit(self, X):
-        self._X = X
-        self._m, self._n = X.shape
-        self._alpha = np.empty(shape=(self._m, self._k))
+        self._X = np.array(X)
+
+        if len(X.shape) > 1:
+            self._m, self._n = X.shape
+        else:
+            self._m = X.shape[0]
+            self._n = 1
+            self._X = self._X.reshape(self._m, self._n)
+        self._alpha = np.ones((self._m, self._k))
+        self._model = (self._alpha, (self._priors, (self._mu, self._sigma)))
 
     def predict(self):
-        pass
+        model_changed = True
+        while model_changed:
+            if (self._alpha < self._epsilon).sum() == 0:
+                gaussians = [norm(mu, sigma) for (mu, sigma) in zip(self._mu, self._sigma)]
+                f = np.array([gaussian.pdf(self._X) for gaussian in gaussians]).T[0]
+                alpha_new = f * self._priors
+                alpha_new = (alpha_new.T / alpha_new.sum(axis=1)).T
+            priors_new = alpha_new.sum(axis=0) / self._m
+            mu_new = (alpha_new.T.dot(self._X).T / alpha_new.sum(axis=0))[0]
+            sigma_new = (alpha_new.T * ((self._X - mu_new)**2).T).T.sum(axis=0) / alpha_new.sum(axis=0)
+
+            new_model = (alpha_new, (priors_new, (mu_new, sigma_new)))
+            if self._compare_models(self._model, new_model):
+                model_changed = False
+                self._gaussians = [norm(mu, sigma) for (mu, sigma) in zip(self._mu, self._sigma)]
+            else:
+                self._alpha = alpha_new
+                self._priors = priors_new
+                self._mu = mu_new
+                self._sigma = sigma_new
+                self._model = new_model
+        return self._priors, (self._mu, self._sigma)
+
+    def compute_gaussian_mixture_pdf(self, x):
+        res = 0
+        for idx, gaussian in enumerate(self._gaussians):
+            res += self._priors[idx] * self._gaussians[idx].pdf(x)
+        return res
+
+    def _compare_models(self, model1, model2):
+        """Returns True if models are equal"""
+        (alpha1, (priors1, (mu1, sigma1))) = model1
+        (alpha2, (priors2, (mu2, sigma2))) = model2
+        return (np.array_equal(alpha1, alpha2) and
+                np.array_equal(priors1, priors2) and
+                np.array_equal(mu1, mu2) and
+                np.array_equal(sigma1, sigma2))
+
--- EM/view.py
+++ EM/view.py
@@ -1,20 +1,35 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy.random as rnd
 from data_generators import generate_data
 from scipy.stats import norm
+from gaussian_mixture import GaussianMixture
 
 k = 2
-mu = [-1, 1]
+mu = [-2.5, 1]
 sigma = [1, 1]
 priors = [.5, .5]
 num_points = 100
 components = [norm(mu[idx], sigma[idx]) for idx in range(k)]
-data, y = generate_data(num_points, 'gaussian', num_states=2, mu=mu, sigma=sigma, priors=priors)
+data, pdfs = generate_data(num_points, 'gaussian', num_states=2, mu=mu, sigma=sigma, priors=priors)
+
+initial_mu = [-1, 1]
+initial_sigma = [1, 1]
+initial_priors = [.5, .5]
+em = GaussianMixture(k, mu=initial_mu, sigma=initial_sigma, priors=initial_priors)
+em.fit(data)
+pred_priors, (pred_mu, pred_sigma) = em.predict()
+print("Predicted priors: ", pred_priors)
+print("Predicted mus: ", pred_mu)
+print("Predicted sigmas: ", pred_sigma)
+
 # plt.scatter(data, np.zeros_like(data), s=10, c=['blue'])
-plt.scatter(data, y, s=10, c=['blue'])
+plt.scatter(data, pdfs, s=10, c=['blue'])
 x = np.linspace(-10, 10, 200)
 for component in components:
     plt.plot(x, component.pdf(x), color='blue')
-plt.xlim(-5, 5)
+# plot gaussian mixture
+plt.plot(x, [em.compute_gaussian_mixture_pdf(point) for point in x], color='red')
+plt.xlim(-10, 10)
 plt.ylim(-.1, .5)
 plt.show()
